eacd1c73400f26ca3bb275340df96ac66166da0a
Sid9879 <singhsiddharth14389@gmail.com>
2025-06-28 18:18:52 +0530

    new

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -48,7 +48,7 @@ const LoginUSer = async(req,res)=>{
          res.cookie("token",token,{
            httpOnly: true,
   secure: true,   
-  sameSite: 'Lax',
+  sameSite: 'None',
   maxAge: 24 * 60 * 60 * 1000
          })
         res.status(200).json({message: "Login successful", success: true});
